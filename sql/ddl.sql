20f724bf1a5a525c943aaa026aa67b4b452c09f5
tmxkqotnl <tmxkqotnl@gmail.com>
2022-04-07 17:57:48 +0900

    fix, sql operations

---
+++ sql/ddl.sql
@@ -0,0 +1,10 @@
+create table recycle_corp_state(
+    region varchar(20),
+    year varchar(4),
+    waste_category varchar(50),
+    classification varchar(30),
+    corp_activated integer,
+    amount_recycled_waste integer,
+    sales integer,
+    total_sales integer
+);
