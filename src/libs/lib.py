66ee45120202c7e527f1d97a73e42810baf9218c
tmxkqotnl <tmxkqotnl@gmail.com>
2022-04-07 13:08:15 +0900

    fix, 작업경과

--- src/libs/lib.py
+++ src/libs/lib.py
@@ -1,4 +1,7 @@
 from os.path import join,dirname,abspath
+import platform
+import matplotlib.pyplot as plt
+
 file_root_dir = join(dirname(abspath(__file__)),'..\\..\\files')
 
 def get_file_dir(dir_name:str,f_name:str=''):
@@ -8,3 +11,13 @@ def get_file_dir(dir_name:str,f_name:str=''):
         return join(file_root_dir,'refined',f_name)
     else:
         return join(file_root_dir,f_name)
+    
+def set_korean():
+    # 한글 폰트 설정
+    if (platform.system() == 'Windows'):
+        plt.rc('font', family='Malgun Gothic')
+    else:
+        plt.rc('font', family='AppleGothic')
+
+    # 음수(-)가 깨지는 현상 방지
+    plt.rcParams['axes.unicode_minus'] = False 
